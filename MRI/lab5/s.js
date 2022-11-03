5e066a10df7a49b649c9e8a5540e317f0f5ad0ad
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2022-11-03 21:56:02 +0100

    Add files via upload

--- MRI/lab5/s.js
+++ MRI/lab5/s.js
@@ -1,29 +1,6 @@
-function logicCalc(x1, x2, x3, x4) {
-   /* let x1 = Boolean(Number(p1));
-    let x2 = Boolean(Number(p2));
-    let x3 = Boolean(Number(p3));
-    let x4 = Boolean(Number(p4));
 
-    let x1x2 = (x1 || x2);
-    let y1 = !(x1x2 & x3);
-    let y2 = !(y1 || (!x4))
-    alert(y1);
-    alert(y2);
-    return [y1, y2];
-*/
-    const x1x2 = new Boolean(x1 || x2);
-    const y1 = new Boolean(!(x1x2 & x3));
-    const y2 = new Boolean(!((!x4) || y1));
-    alert(y1);
-    alert(y2);
-
-  }
-  /*function add(a,b) {
-    const logic = new Boolean(a & b);
-    alert(logic);
-  }*/
   function logic(a, b, c, d) {
-    //var sum = parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10) + parseInt(d, 10);
+
     let x1 = Boolean(Number(a));
     let x2 = Boolean(Number(b));
     let x3 = Boolean(Number(c));
@@ -33,7 +10,27 @@ function logicCalc(x1, x2, x3, x4) {
     let y1 = !(x1x2 & x3);
     let y2 = !(y1 || (!x4))
 
-      alert("y1= " +y1 + ", y2 =" +y2);
+      if(a>1 || a<0)
+      {
+        alert("Prosze podac 0 albo 1");
+      }
+      else if(b>1 || b<0)
+      {
+        alert("Prosze podac 0 albo 1");
+      }
+      else if(c>1 || c<0)
+      {
+        alert("Prosze podac 0 albo 1");
+      }
+      else if(d>1 || d<0)
+      {
+        alert("Prosze podac 0 albo 1");
+      }
+      else
+      {
+        alert("y1= " +y1 + ", y2 =" +y2);
+        return [y1, y2];
+      }
 
     
   }
