7b78b8d2dc32f85a8a629f182cf047d5155b5328
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2022-12-22 10:34:03 +0100

    Add files via upload

---
+++ MRI/lab10/script1.js
@@ -0,0 +1,15 @@
+alert("Wesołych świąt\nNaciśnij aby puścić muzyczke");
+
+
+const audio = document.getElementById('song'); const p = document.querySelector('p'); window.onclick = function(){ audio.volume = 1; audio.play(); p.style.display = "none"; }
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  
